326d5304f63e544b79a6661eface809d6d8d7b16
Shubhammawa <shubhammawa1998@gmail.com>
2018-05-18 06:23:55 +0530

    Added RandomizedSearchCV

--- sklearn_test8.py
+++ sklearn_test8.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import cross_val_score
 import matplotlib.pyplot as plt 
 from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import RandomizedSearchCV
 
 iris = load_iris()
 
@@ -17,6 +18,9 @@ print(scores.mean())
 
 k_range = range(1,31)
 print(k_range)
+
+# Manually finding best paramters
+
 #k_scores = []
 #for k in k_range:
 #	knn = KNeighborsClassifier(n_neighbors=k)
@@ -29,33 +33,53 @@ print(k_range)
 #plt.ylabel("Cross-validated Accuracy")
 #plt.show()
 
-param_grid = dict(n_neighbors=k_range)
-print(param_grid)
+# Using GridSearchCV for paramter tuning
 
-grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy', n_jobs = -1)
+#param_grid = dict(n_neighbors=k_range)
+#print(param_grid)
 
-grid.fit(X,y)
+#grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy', n_jobs = -1)
 
-grid.grid_scores_
+#grid.fit(X,y)
 
-print(grid.grid_scores_[0].parameters)
-print(grid.grid_scores_[0].cv_validation_scores)
-print(grid.grid_scores_[0].mean_validation_score)
+#grid.grid_scores_
 
-grid_mean_scores = [result.mean_validation_score for result in grid.grid_scores_]
+#print(grid.grid_scores_[0].parameters)
+#print(grid.grid_scores_[0].cv_validation_scores)
+#print(grid.grid_scores_[0].mean_validation_score)
 
-plt.plot(k_range,grid_mean_scores)
-plt.xlabel('Value of K for KNN')
-plt.ylabel("Cross-validated Accuracy")
-plt.show()
+#grid_mean_scores = [result.mean_validation_score for result in grid.grid_scores_]
 
-print(grid.best_score_)
-print(grid.best_params_)
-print(grid.best_estimator_)
+#plt.plot(k_range,grid_mean_scores)
+#plt.xlabel('Value of K for KNN')
+#plt.ylabel("Cross-validated Accuracy")
+#plt.show()
+
+#print(grid.best_score_)
+#print(grid.best_params_)
+#print(grid.best_estimator_)
 
 # Best parameter found to be n_neighors = 13
 # Predicting using this paramter
-knn = KNeighborsClassifier(n_neighbors=13)
-knn.fit(X,y)
+#knn = KNeighborsClassifier(n_neighbors=13)
+#knn.fit(X,y)
 
-print(knn.predict([3,5,4,2]))
+#print(knn.predict([3,5,4,2]))
+
+# Using RandomizedSearchCV for paramter tuning
+
+param_dist = dict(n_neighbors=k_range)
+
+rand = RandomizedSearchCV(knn, param_dist, cv=10, scoring='accuracy',n_iter=10, random_state=5)
+rand.fit(X,y)
+rand.grid_scores_
+
+print(rand.best_score_)
+print(rand.best_params_)
+
+best_scores = []
+for _ in range(20):
+	rand = RandomizedSearchCV(knn, param_dist, cv=10,scoring='accuracy',n_iter=10)
+	rand.fit(X,y)
+	best_scores.append(round(rand.best_score_,3))
+print(best_scores)
