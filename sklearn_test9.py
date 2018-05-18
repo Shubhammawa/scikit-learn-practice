b0b943906c42294ebcb405aa90c73852d95681c5
Shubhammawa <shubhammawa1998@gmail.com>
2018-05-19 01:49:49 +0530

    Added confusion matrix

--- sklearn_test9.py
+++ sklearn_test9.py
@@ -15,14 +15,29 @@ y = pima.label
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-logreg = LogisticRegression()
+logreg = LogisticRegres	sion()
 logreg.fit(X_train, y_train)
 
 y_pred_class = logreg.predict(X_test)
 print metrics.accuracy_score(y_test, y_pred_class)
 
+# Metrics computed from confusion matrix
+
+# Senstivity
+# TP/(TP + FN)
+
+# Specificity 
+# TN/(TN + FP)
+
+# Fasle positive rate
+# FP/(TN + FP)
+
+# Precision
+# TP/(TP + FP)
 # Calculating null accuracy
 # y_test.value_counts() - Gives number of zeroes and ones
 # y_test.mean()	- Percentage of ones
 # 1 - y_test.mean() - Percentage of zeroes
 # Null_accuracy = max(y_test.mean(), 1 - y_test.mean())
+
+print(metrics.confusion_matrix(y_test, y_pred_class))
